947e7e2a68ac74bd52f2eaa53f0ef6c915bbf741
Anderson Guzman <62262763+AndGuz@users.noreply.github.com>
2021-12-04 16:12:35 -0400

    Update #5.py

--- #5.py
+++ #5.py
@@ -1,4 +1,4 @@
-#5- Crea un programa que calcule el área y el perímetro de un triangulo
+#5 Crea un programa que calcule el área y el perímetro de un triangulo
 import os
 import time
 def limpiar():
