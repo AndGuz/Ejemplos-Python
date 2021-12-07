2ef4ccc4d688aaa94d31b3df6fe6349a4e719b18
Anderson Guzman <62262763+AndGuz@users.noreply.github.com>
2021-12-07 11:56:30 -0400

    Media geometrica

---
+++ Media geometrica de 3 numeros.py
@@ -0,0 +1,27 @@
+import os
+import time
+
+
+def limpiar() -> None:
+    os.system("cls" if os.name == "nt" else "clear")
+
+#La raiz cubica se saca elevando x por la division de 1/3 que es un tercio
+def qrt(x) -> float:
+    return x**(1./3.)
+
+def geo_media(n1,n2,n3) -> float:
+    return qrt((n1*n2*n3))
+
+def main() -> None:
+    print("""
+Hola!
+Bienvenido al calculador de media geometrica. """)
+    x1 = float(input("Inserte la primera variable: "))
+    x2 = float(input("Inserte la segunda variable: "))
+    x3 = float(input("Inserte la tercera variable: "))
+    print(f"La media geometrica es: {geo_media(x1,x2,x3)}")
+    time.sleep(5)
+    os._exit(0)
+
+if __name__ == "__main__":
+    main()
--- Nuevo salario si obtuvo algun incremento.py
+++ Nuevo salario si obtuvo algun incremento.py
@@ -2,14 +2,20 @@ import os
 import time
 
 def limpiar():
-    os.system("cls" if os.name is "nt" else "clear")
+    os.system("cls" if os.name == "nt" else "clear")
 
 def porcentaje(valor,sueldo) -> float:
     por = sueldo * (valor / 100)
     return por
 
 def main():
-    pass
+    limpiar()
+    sueldo_act = float(input("Inserte salario actual: "))
+    porc = float(input("Inserte incremento de porciento: "))
+    sueldo_nuev = porcentaje(porc,sueldo_act) + sueldo_act
+    print(f"Su sueldo sera de: {sueldo_nuev}. ")
+    time.sleep(5)
+    os._exit(0)
 
 if __name__ == "__main__":
     main()
